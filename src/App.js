3675f7661f533265d64ea14f3cccedf496a82225
Reda IBENAHMED <ibenahmed.reda05@gmail.com>
2024-05-02 22:40:36 +0100

    change pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Home, Profile, About, Contact } from './pages/index';
 function App() {
     return (
         <>
-            <BrowserRouter basename={'/CYBORG'}>
+            <BrowserRouter basename={'/CYBORGS'}>
                 <Header />
                 <Container>
                     <Routes>
